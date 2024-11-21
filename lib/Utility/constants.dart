43435ad2672353a23236b02a742fae911248dc61
Imperial79 <avishekverma79@gmail.com>
2024-11-21 21:11:49 +0530

    version upgraded

--- lib/Utility/constants.dart
+++ lib/Utility/constants.dart
@@ -1,7 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
 
-const String kAppVersion = "2.7.1";
+const String kAppVersion = "2.7.2";
 
 class FirebaseRefs {
   static final _firestore = FirebaseFirestore.instance;
